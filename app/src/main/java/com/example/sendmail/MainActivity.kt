8298e013f1d489958cff64d5220eedc11a33611a
Brun0HC <bruno.cobuci@biotronica.tech>
2024-05-11 12:57:29 -0300

    feat: implement send email activity

--- app/src/main/java/com/example/sendmail/MainActivity.kt
+++ app/src/main/java/com/example/sendmail/MainActivity.kt
@@ -1,5 +1,8 @@
 package com.example.sendmail
 
+import android.content.Intent
+import android.content.Intent.*
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.sendmail.databinding.ActivityMainBinding
@@ -13,5 +16,33 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(amb.root)
+
+        amb.cleanBt.setOnClickListener {
+            with (amb) {
+                toEt.setText("")
+                ccEt.setText("")
+                bccEt.setText("")
+                subjectEt.setText("")
+                messageEt.setText("")
+            }
+        }
+
+        amb.sendBt.setOnClickListener {
+            val sendMailIntent = Intent(ACTION_SENDTO)
+
+            with(sendMailIntent){
+                putExtra(EXTRA_EMAIL, arrayOf(amb.toEt.text.toString()))
+                putExtra(EXTRA_CC, arrayOf(amb.ccEt.text.toString()))
+                putExtra(EXTRA_BCC, arrayOf(amb.bccEt.text.toString()))
+                putExtra(EXTRA_SUBJECT, arrayOf(amb.subjectEt.text.toString()))
+                putExtra(EXTRA_TEXT, arrayOf(amb.messageEt.text.toString()))
+                type = "message/rfc822"
+                data = Uri.parse("mailto:")
+            }
+
+            val chooserIntent = Intent(ACTION_CHOOSER)
+            chooserIntent.putExtra(EXTRA_INTENT, sendMailIntent)
+            startActivity(chooserIntent)
+        }
     }
 }
